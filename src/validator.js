29b1222dedb5c5809cd0d78d7e8eb1a59c8076c5
Adam Nalisnick <theadam4257@gmail.com>
2014-08-14 20:46:32 -0400

    fixing a bug and updating version

--- src/validator.js
+++ src/validator.js
@@ -4,21 +4,22 @@ var validMatcher = /^(is.*|equals|contains|matches)/;
 var v = {};
 
 var getCurriedFunction = function(vfunc){
-    return function(){
-        var args = Array.prototype.slice.call(arguments, 0);
-        return function(value){
-            args.unshift(value);
-            return vfunc.apply(nv, args);
-        };
+  return function(){
+    var initialArgs = Array.prototype.slice.call(arguments, 0);
+    return function(value){
+      args = initialArgs.slice(0);
+      args.unshift(value);
+      return vfunc.apply(nv, args);
     };
+  };
 };
 
 for(var key in nv){
-    if(!nv.hasOwnProperty(key)) continue;
-    if(typeof nv[key] !== 'function') continue; // only care about functions
+  if(!nv.hasOwnProperty(key)) continue;
+  if(typeof nv[key] !== 'function') continue; // only care about functions
     if(!validMatcher.test(key)) continue;
 
     v[key] = getCurriedFunction(nv[key]);
-}
+  }
 
-module.exports = exports = v;
+  module.exports = exports = v;
